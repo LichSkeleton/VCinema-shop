837ec485be29fb498c7a179ee82fe4e07bc52041
Vladick <96355698+LichSkeleton@users.noreply.github.com>
2023-06-15 11:30:43 +0300

    Add project files.

---
+++ VCinema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VCinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
