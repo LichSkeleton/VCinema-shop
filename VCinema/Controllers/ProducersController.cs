0c6cc242a8d162910bfc49f804cddd7664bb4a59
Vladick <96355698+LichSkeleton@users.noreply.github.com>
2023-07-04 22:59:45 +0300

    Added update producer

--- VCinema/Controllers/ProducersController.cs
+++ VCinema/Controllers/ProducersController.cs
@@ -46,5 +46,29 @@ namespace VCinema.Controllers
             await _service.AddAsync(producer);
             return RedirectToAction(nameof(Index));
         }
+        // Get: producers/Edit
+        public async Task<IActionResult> Edit(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Producer producer)
+        {
+            // Check for validation errors only on relevant properties
+            ModelState.TryGetValue("Id", out var idError);
+            ModelState.TryGetValue("FullName", out var fullNameError);
+            ModelState.TryGetValue("ProfilePictureURL", out var profilePictureUrlError);
+            ModelState.TryGetValue("Bio", out var bioError);
+
+            if (idError == null || fullNameError == null || profilePictureUrlError == null || bioError == null) return View(producer);
+            if (id == producer.Id)
+            {
+                await _service.UpdateAsync(id, producer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
+        }
     }
 }
