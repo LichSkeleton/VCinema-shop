d5e76a2847f8c1d1aeab8ec543e81e8c25af2cde
Vladick <96355698+LichSkeleton@users.noreply.github.com>
2023-07-04 23:39:43 +0300

    Update cinema data

--- VCinema/Controllers/CinemasController.cs
+++ VCinema/Controllers/CinemasController.cs
@@ -46,5 +46,28 @@ namespace VCinema.Controllers
             if (cinemaDetails == null) return View("NotFound");
             return View(cinemaDetails);
         }
+        // Get: Actors/Edit
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Name,Description")] Cinema cinema)
+        {
+            // Check for validation errors only on relevant properties
+            ModelState.TryGetValue("Id", out var idError);
+            ModelState.TryGetValue("Name", out var nameError);
+            ModelState.TryGetValue("Logo", out var logoError);
+            ModelState.TryGetValue("Description", out var descriptionError);
+
+            if (idError==null || nameError == null || logoError == null || descriptionError == null)
+            {
+                return View(cinema);
+            }
+            await _service.UpdateAsync(id, cinema);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
