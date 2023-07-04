2b4c43fa7fe38a4df3764806024b0184a59e4537
Vladick <96355698+LichSkeleton@users.noreply.github.com>
2023-07-04 22:19:49 +0300

    Курс всже все

--- VCinema/Data/Services/IActorsService.cs
+++ VCinema/Data/Services/IActorsService.cs
@@ -3,15 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VCinema.Data.Base;
 
 namespace VCinema.Data.Services
 {
-    public interface IActorsService
+    public interface IActorsService:IEntityBaseRepository<Actor>
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);
-        Task AddAsync(Actor actor);
-        Task<Actor> UpdateAsync(int id,Actor newActor);
-        Task DeleteAsync(int id);
     }
 }
--- VCinema/Models/Actor.cs
+++ VCinema/Models/Actor.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using VCinema.Data.Base;
 
 namespace VCinema.Models
 {
-    public class Actor
+    public class Actor : IEntityBase
     {
         [Key]
         public int Id { get; set; }
