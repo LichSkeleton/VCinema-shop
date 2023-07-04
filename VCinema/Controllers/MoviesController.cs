f8a646dbdfe0e02f81cb0c2991663d6ca7ea6b1c
Vladick <96355698+LichSkeleton@users.noreply.github.com>
2023-07-05 00:08:55 +0300

    added create view + viewdata + viewbag

--- VCinema/Controllers/MoviesController.cs
+++ VCinema/Controllers/MoviesController.cs
@@ -24,5 +24,14 @@ namespace VCinema.Controllers
             var movieDetail = await _service.GetMovieByIdAsync(id);
             return View(movieDetail);
         }
+
+        //GET: Movies/Create
+        public IActionResult Create()
+        {
+            ViewData["Welcome"] = "Welcome to our store";
+            ViewBag.Description = "This is the store description";
+
+            return View();
+        }
     }
 }
