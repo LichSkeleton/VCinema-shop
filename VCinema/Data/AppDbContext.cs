600e9348da4b4c832a29f3a91fe03198a88c0738
Vladick <96355698+LichSkeleton@users.noreply.github.com>
2023-07-05 06:53:25 +0300

    added order and order items

--- VCinema/Data/AppDbContext.cs
+++ VCinema/Data/AppDbContext.cs
@@ -5,9 +5,9 @@ namespace VCinema.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
+
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -26,5 +26,9 @@ namespace VCinema.Data
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
+
+        //Orders related tables
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
