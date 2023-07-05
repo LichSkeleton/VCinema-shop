2208038177499b15bd051ff709b6fb9d8ad03851
Vladick <96355698+LichSkeleton@users.noreply.github.com>
2023-07-05 08:02:26 +0300

    added orders list

--- VCinema/Controllers/OrdersController.cs
+++ VCinema/Controllers/OrdersController.cs
@@ -17,7 +17,13 @@ namespace VCinema.Controllers
             _shoppingCart = shoppingCart;
             _ordersService = ordersService;
         }
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
 
+            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            return View(orders);
+        }
         public IActionResult ShoppingCart()
         {
             var items = _shoppingCart.GetShoppingCartItems();
