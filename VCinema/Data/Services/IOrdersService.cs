bcc660f0b09322d6d83f6714aa271a0c4aa13e94
Vladick <96355698+LichSkeleton@users.noreply.github.com>
2023-07-05 07:33:53 +0300

    added and configured ordersservice

---
+++ VCinema/Data/Services/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using VCinema.Models;
+
+namespace VCinema.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+    }
+}
